52ce732da447fc258a389dac9cb9a950df10e6ca
koh1 <koh01.amy@gmail.com>
2014-09-04 18:12:13 +0900

    modified:   views.py

--- analyzer/views.py
+++ analyzer/views.py
@@ -86,6 +86,27 @@ def nwk_index(request, sim_id):
     return HttpResponse(t.render(c))
 
 
+def get_nwk_chart_img(request):
+    sim_id = request.POST['sim_id']
+    nwk_name = request.POST['nwk_name']
+    result = SimulationResult.objects.filter(sim_id=sim_id)
+    if len(result) > 0:
+        dbinfo = ResultSourceMongodb.objects.get(id=result[0].result_source_mongodb.id)
+        db = pymongo.Connection(dbinfo.host, dbinfo.port)[result[0].db_name]
+
+        ## for older version
+        enwcoll = db["%s_edge_nwk" % sim_id]
+        
+        df = pd.DataFrame(list(enwcoll.find({"nwk_name": nwk_name})))
+        ret_x = list(df[request.POST['x']])
+        ret_y = list(df[request.POST['y']])
+        xdata = {'x': ret_x}
+        ydata = {'y': ret_y}
+        ret.append(xdata)
+        ret.append(ydata)
+        logger.info(ret)
+
+
 def get_nwk_chart_data(request):
     sim_id = request.POST['sim_id']
     nwk_name = request.POST['nwk_name']
