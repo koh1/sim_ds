482c830a3001b833040ac2ff649af7ad854a5bf9
koh1 <koh01.amy@gmail.com>
2014-11-10 16:01:01 +0900

    modified:   result_manager/tasks.py new file:   sim_dashboard/celery.py modified:   sim_dashboard/settings.py modified:   templates/base.html

--- result_manager/tasks.py
+++ result_manager/tasks.py
@@ -14,4 +14,10 @@ def add(x, y):
     logger.info("Adding %s + %s" % (x, y))
     return x + y
 
-
+@task
+def exec_d2xp_mbs(conf, route, nd_spec, nw_def, area_def):
+    logger = Task.get_logger()
+    
+@task
+def exec_mbs(conf):
+    logger = Task.get_logger()
