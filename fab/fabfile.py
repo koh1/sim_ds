95b86f5e505fbda891d249044cbcf9b7119303e7
kame <kame@paeonia.local>
2014-12-23 23:56:44 +0900

    modified:   fabfile.py new file:   files/id_rsa_sim_ds_worker new file:   files/id_rsa_sim_ds_worker.pub

--- fab/fabfile.py
+++ fab/fabfile.py
@@ -1,10 +1,17 @@
-from fabric.api import run, sudo, cd, lcd, local
+from fabric.api import run, sudo, cd, lcd, local, put, prompt
 
 def deploy_worker():
     pass
 
-def deploy_sshkey():
-    pass
+def deploy_sshkey(ssh_key=""):
+    if ssh_key == "":
+        ssh_key = prompt("Specify ssh public key file:" )
+    
+    put(ssh_key, '~/.ssh/id_rsa.pub')
+    run('cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys')
+    run('chmod 600 ~/.ssh/authorized_keys')
+    
+
 
 
 
