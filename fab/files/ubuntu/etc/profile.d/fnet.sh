af69a5edcb3df12c1e77079b55876937c1f79e64
koh1 <koh01.amy@gmail.com>
2015-01-15 20:48:42 +0900

    new file:   fab/files/ubuntu/etc/apt/apt.conf new file:   fab/files/ubuntu/etc/profile.d/fnet.sh new file:   fab/files/ubuntu/etc/resolvconf/resolv.conf.d/base

---
+++ fab/files/ubuntu/etc/profile.d/fnet.sh
@@ -0,0 +1,3 @@
+export ftp_proxy=http://hydrangeaproxy:1982166878@rep.proxy.nic.fujitsu.com:8080
+export http_proxy=http://hydrangeaproxy:1982166878@rep.proxy.nic.fujitsu.com:8080
+export https_proxy=http://hydrangeaproxy:1982166878@rep.proxy.nic.fujitsu.com:8080
