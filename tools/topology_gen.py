1690067e5d3b8366519e6587aa039773ad1fb125
koh1 <koh01.amy@gmail.com>
2015-01-26 22:43:40 +0900

    modified:   tools/topology_gen.py

--- tools/topology_gen.py
+++ tools/topology_gen.py
@@ -15,92 +15,108 @@ def convert_for_d3_make_children(parent, p_name, data):
 
 
 def which_is_parent(o1, o2):
-    pass
+    if o1["type"] > o2["type"]:
+        return 0
+    elif o1["type"] < o2["type"]:
+        return 1
+    else:
+        if o1["level"] > o2["level"]:
+            return 1
+        elif o1["level"] < o2["level"]:
+            return 0
+        else:
+            # error
+            return -1 
+
+def add_topology_entry(topology, entry):
+    topology[entry["name"]] = {}
+    topology[entry["name"]]["name"] = entry["name"]
+    topology[entry["name"]]["level"] = entry["level"]
+    topology[entry["name"]]["children"] = {}
+    
 
 def get_topology_data_proto(csv_data):
     import numpy as np
 
     topology = {}
-    for row in routes:
+    for row in csv_data:
         src = get_component_object(row[0])
-        if topology[src["name"]] = None:
-            topology[src["name"]] = {}
-            topology[src["name"]]["name"] = src["name"]
-            topology[src["name"]]["level"] = src["level"]
-            topology[src["name"]]["children"] = {}
+        if not topology.has_key(src["name"]):
+            add_topology_entry(topology, src)
 
         dst = get_component_object(row[1])
-        if topology[dst["name"]] = None:
-            topology[dst["name"]] = {}
-            topology[dst["name"]]["name"] = src["name"]
-            topology[dst["name"]]["level"] = src["level"]
-            topology[dst["name"]]["children"] = {}
+        if not topology.has_key(dst["name"]):
+            add_topology_entry(topology, dst)
 
         if len(row) == 2:
-            if src["type"] > dst["type"]:
+            v = which_is_parent(src, dst)
+            if v == 0:
                 topology[src["name"]]["children"][dst["name"]] = 1
-            else:
+            elif v == 1:
                 topology[dst["name"]]["children"][src["name"]] = 1
+            else:
+                pass
         elif len(row) == 3:
-            pass
+            mid = get_component_object(row[2])
+            if not topology.has_key(mid["name"]):
+                add_topology_entry(topology, mid)
+            v = which_is_parent(src, mid)
+            if v == 0:
+                topology[src["name"]]["children"][mid["name"]] = 1
+            elif v== 1:
+                topology[mid["name"]]["children"][src["name"]] = 1
+
+            v = which_is_parent(mid, dst)
+            if v == 0:
+                topology[mid["name"]]["children"][dst["name"]] = 1
+            elif v == 1:
+                topology[dst["name"]]["children"][mid["name"]] = 1
+            else:
+                pass
         else:
             prev_via = src
             via = None
             for i in np.arange(len(row) - 3) + 2:
                 via = get_component_object(row[i])
-                if topology[via["name"]] = None:
-                    topology[via["name"]] = {}
-                    topology[via["name"]]["name"] = via["name"]
-                    topology[via["name"]]["level"] = via["level"]
-                    topology[via["name"]]["children"] = {}
+                if topology.has_key(via["name"]):
+                    add_topology_entry(topology, via)
                 
-                if prev_via["type"] > via["type"]:
+                v = which_is_parent(prev_via, via)
+                if v == 0:
                     topology[prev_via["name"]]["children"][via["name"]] = 1
-                else:
+                elif v == 1:
                     topology[via["name"]]["children"][prev_via["name"]] = 1
+                else:
+                    pass
                 
                 prev_via = via
             
             row_tail = get_component_object(row[-1])
-            if topology[row_tail["name"]] = None:
-                topology[row_tail["name"]] = {}
-                topology[row_tail["name"]]["name"] = row_tail["name"]
-                topology[row_tail["name"]]["level"] = row_tail["level"]
-                topology[row_tail["name"]]["children"] = {}
-            if row_tail["type"] > dst["type"]:
+            if topology.has_key(row_tail["name"]):
+                add_topology_entry(topology, row_tail)
+            v = which_is_parent(row_tail, dst)
+            if v == 0:
                 topology[row_tail["name"]]["children"][dst["name"]] = 1
-            else:
+            elif v == 1:
                 topology[dst["name"]]["children"][row_tail["name"]] = 1
+            else:
+                pass
                 
-        each_route = {}
-        each_route['src'] = {
-            "name": row[0],
-            "type": row[0].split('-')[0],
-            "level": int(row[0].split('-')[1][-1])
-            }
-        each_route['dst'] = {
-            "name": row[1],
-            "type": row[1].split('-')[0],
-            "level": int(row[1].split('-')[1][-1])
-            }
-        
-        if len(row) > 2:
-            via = row[2:-1]
-            
-        else:
-            pass
+    return topology
 
-
-    
 def get_component_object(component_name):
     obj = {}
     obj["name"] = component_name
-    split_name = component_name.split('-')
-    obj["level"] = split_name[1][-1]
-    if split_name[0] == "SW":
-        obj["type"] = 1
-    else:
+    if obj["name"] == "CL":
+        obj["level"] = 100
         obj["type"] = 0
+    else:
+        split_name = component_name.split('-')
+        obj["level"] = split_name[1][-1]
+        if split_name[0] == "SW":
+            obj["type"] = 1
+        else:
+            obj["type"] = 0
 
     return obj
     
@@ -108,6 +124,7 @@ def get_component_object(component_name):
 if __name__ == '__main__':
     import json
     import sys
+    import csv
 
     p = sys.argv
     if len(p) < 2:
@@ -115,8 +132,10 @@ if __name__ == '__main__':
         sys.exit(0)
     
     
-    topo_org = json.loads(open(p[1]).read())
-    res = convert_for_d3(topo_org)
+#    topo_org = json.loads(open(p[1]).read())
+    routing_csv = csv.reader(open(p[1]), delimiter=',')
+#    res = convert_for_d3(topo_org)
+    res = get_topology_data_proto(routing_csv)
     
     f = open("outout.json", 'w')
     f.write(json.dumps(res))
